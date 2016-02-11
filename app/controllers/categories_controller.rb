5e8588d3436949f095f877c8db697e9102ed525c
Namrata Soni <namrata.soni@solutelabs.com>
2016-02-11 18:46:15 +0530

    Updated

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user! 
   # GET /categories
   # GET /categories.json
   def index
@@ -70,6 +70,8 @@ class CategoriesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_category
       @category = Category.find(params[:id])
+
+
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'dashboard' => 'home#dashboard'
   get 'categories'  => 'categories#show'
   get 'products'  => 'products#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
